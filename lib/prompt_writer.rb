93af169de16dd217dca368c6adacc448b729c8f7
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-01-11 18:42:07 +0000

    Prints board with occupied cells

--- lib/prompt_writer.rb
+++ lib/prompt_writer.rb
@@ -6,33 +6,55 @@ class PromptWriter
   end
 
   def ask_for_next_move
-    @std_out.puts "Please enter your next move"
+    std_out.puts "Please enter your next move"
   end
 
   def show_winning_message
-    @std_out.puts "The game has been won!"
+    std_out.puts "The game has been won!"
   end
 
   def show_draw_message 
-    @std_out.puts "The game is a draw!"
+    std_out.puts "The game is a draw!"
   end
 
   def show_board(board)
     board_to_display = ""
     cells = board.grid
+
     cells.each_index do |index|
-      if cells[index].nil?
-        puts " the current index is: " + index.to_s
-      end
-      cell_value = index + 1
-      board_to_display = board_to_display + " | " + cell_value.to_s
+      board_to_display = board_to_display + display_cell(cells[index], index) 
 
-      if (index + 1) % 3 == 0
-        board_to_display = board_to_display + " |\n"
-      end
-
+      #  if !cells[index].nil?
+      #    board_to_display = board_to_display + divider + cells[index].to_s   
+      #  else 
+      #    board_to_display = board_to_display + divider + one_based(index).to_s
+      board_to_display += add_new_line_if_end_of_row(index)
     end
-    puts "board is: " + board_to_display.to_s
-    @std_out.puts board_to_display
+    std_out.puts board_to_display
+  end
+
+  private
+
+  def one_based(index)
+    index + 1
+  end
+
+  def divider
+    " | " 
+  end
+
+  def add_new_line_if_end_of_row(index)
+    if (index + 1) % 3 == 0
+      return " |\n"
+    end
+    return ""
+  end
+
+  def display_cell(content, index)
+    if !content.nil?
+      return divider + content.to_s   
+    else 
+      return divider + one_based(index).to_s
+    end
   end
 end
--- spec/prompt_writer_spec.rb
+++ spec/prompt_writer_spec.rb
@@ -27,6 +27,11 @@ RSpec.describe PromptWriter do
     board = Board.new
     @prompt.show_board(board)
     expect(@std_out.string).to eq(" | 1 | 2 | 3 |\n | 4 | 5 | 6 |\n | 7 | 8 | 9 |\n")
+  end
 
+  it "displays board with occupied cells" do
+    board = Board.new([:X, nil, nil, :O, :X, nil, nil, nil, nil])
+    @prompt.show_board(board)
+    expect(@std_out.string).to eq(" | X | 2 | 3 |\n | O | X | 6 |\n | 7 | 8 | 9 |\n")
   end
 end
