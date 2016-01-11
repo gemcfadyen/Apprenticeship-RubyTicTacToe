ed364006896282d32138b904a22724b51a4e2cbe
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-01-11 17:14:17 +0000

    Prints win and draw message to console

--- lib/prompt_writer.rb
+++ lib/prompt_writer.rb
@@ -6,7 +6,14 @@ class PromptWriter
   end
 
   def ask_for_next_move
-    std_out.puts "Please enter your next move"
+   @std_out.puts "Please enter your next move"
   end
 
+  def show_winning_message
+    @std_out.puts "The game has been won!"
+  end
+
+  def show_draw_message 
+    @std_out.puts "The game is a draw!"
+  end
 end
--- spec/prompt_writer_spec.rb
+++ spec/prompt_writer_spec.rb
@@ -1,12 +1,24 @@
 require 'prompt_writer'
 
 RSpec.describe PromptWriter do
+  before (:each) do
+    @std_out = StringIO.new
+    @prompt = PromptWriter.new(@std_out)
+  end
 
   it "prompts for an input" do
-    std_out = StringIO.new 
-    prompt = PromptWriter.new(std_out)
-    prompt.ask_for_next_move
+    @prompt.ask_for_next_move
 
-    expect(std_out.string).to eq "Please enter your next move\n"
+    expect(@std_out.string).to eq "Please enter your next move\n"
+  end
+
+  it "displays winning message" do
+    @prompt.show_winning_message 
+    expect(@std_out.string).to eq "The game has been won!\n"
+  end
+
+  it "displays draw message" do
+    @prompt.show_draw_message
+    expect(@std_out.string).to eq "The game is a draw!\n"
   end
 end
