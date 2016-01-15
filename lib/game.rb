d64a309f9d395fe7cbd737e5fd7b943014bca198
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-01-15 16:45:50 +0000

    board ends after winning row formed

--- lib/game.rb
+++ lib/game.rb
@@ -7,9 +7,13 @@ class Game
 
   def play 
     current_player_index = 0
-    while(board.free_spaces? && !board.winning_combination?)
+    p board.grid_for_display
+
+    while game_in_progress
       current_player = players[current_player_index]
       @board = board.make_move(current_player.choose_move(board), current_player.game_symbol)  
+      p board.grid_for_display 
+      
       current_player_index = toggle_player(current_player_index)
     end
   end
@@ -18,8 +22,13 @@ class Game
 
   attr_reader :board, :players
 
+  def game_in_progress
+    inprog = board.free_spaces? && !board.winning_combination?
+    p inprog
+    inprog
+  end
+
   def toggle_player(current_player_index)
     current_player_index == 1 ? 0 : 1
   end
-
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,22 +4,29 @@ require 'player_symbols'
 require 'game'
 
 RSpec.describe Game do
-  before(:each) do
-    @player_x_spy = instance_double("HumanPlayer")
-    expect(@player_x_spy).to receive(:game_symbol).and_return(PlayerSymbols::X).exactly(5).times
-    @player_o_spy = instance_double("HumanPlayer")
-    expect(@player_o_spy).to receive(:game_symbol).and_return(PlayerSymbols::O).exactly(4).times
-  end
+  let(:player_x_spy) { instance_double(HumanPlayer).as_null_object }
+  let(:player_o_spy) { instance_double(HumanPlayer).as_null_object }
 
   it "players take turn until there is no space on board" do
-    expect(@player_x_spy).to receive(:choose_move).exactly(5).times.and_return(0, 1, 4, 5, 6)
-    expect(@player_o_spy).to receive(:choose_move).exactly(4).times.and_return(2, 3, 7, 8)
-    game = Game.new(Board.new, [@player_x_spy, @player_o_spy])
+    expect(player_x_spy).to receive(:choose_move).exactly(5).times.and_return(0, 1, 4, 5, 6)
+    expect(player_o_spy).to receive(:choose_move).exactly(4).times.and_return(2, 3, 7, 8)
+
+    game = Game.new(Board.new, [player_x_spy, player_o_spy])
     game.play
   end
 
-#  it "game is won when there is a winning combination in the board" do
-#    winning_board =  Board.new([X, X, X, nil, nil, O, O, nil])
-#  end
-#
+  it "game ends when a winning combination is formed" do
+    expect(player_x_spy).to receive(:choose_move).once.and_return(2)
+    allow(player_x_spy).to receive(:game_symbol).and_return(PlayerSymbols::X)
+
+    board = Board.new([PlayerSymbols::X, PlayerSymbols::X, nil, PlayerSymbols::O, nil, nil, nil, nil, nil])
+    game = Game.new(board, [player_x_spy, player_o_spy])
+
+    game.play
+  end
+
+  #  it "game is won when there is a winning combination in the board" do
+  #    winning_board =  Board.new([X, X, X, nil, nil, O, O, nil])
+  #  end
+  #
 end
