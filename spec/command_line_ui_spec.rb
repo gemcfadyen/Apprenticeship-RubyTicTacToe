6c1500730e306064c98c8190cbdf8f7146295f39
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-01-15 17:04:03 +0000

    Updates the expectations to be in the past on the command line ui

--- spec/command_line_ui_spec.rb
+++ spec/command_line_ui_spec.rb
@@ -15,97 +15,105 @@ RSpec.describe CommandLineUI do
 
     command_line_ui.get_move_from_player(Board.new)
 
-    expect(writer_spy).to respond_to(:ask_for_next_move)
+    expect(writer_spy).to have_received(:ask_for_next_move)
   end
 
   it "reads move from user" do
     allow(reader_spy).to receive(:get_input).and_return("1")
-
-    expect(reader_spy).to receive(:get_input).and_return("1")
 
     expect(command_line_ui.get_move_from_player(Board.new)).to be 1 
   end
 
   it "shows board when asking for move" do
     allow(reader_spy).to receive(:get_input).and_return("1")
-    expect(writer_spy).to receive(:show_board).once
 
     command_line_ui.get_move_from_player(Board.new)
+
+    expect(writer_spy).to have_received(:show_board)
   end
 
   it "displays error message when reprompted" do
     allow(reader_spy).to receive(:get_input).and_return("a", "3")
-    expect(writer_spy).to receive(:error_message)
-  
+
     command_line_ui.get_move_from_player(Board.new)  
+
+    expect(writer_spy).to have_received(:error_message)
   end
 
   it "displays board with every reprompt" do
     allow(reader_spy).to receive(:get_input).and_return("a", "3")
-    expect(writer_spy).to receive(:show_board).twice
 
     command_line_ui.get_move_from_player(Board.new)  
+
+    expect(writer_spy).to have_received(:show_board).twice
   end
 
   it "reprompts move from user when an alpha character entered" do
     allow(reader_spy).to receive(:get_input).and_return("a", "3")
-    expect(writer_spy).to receive(:ask_for_next_move).twice
 
     expect(command_line_ui.get_move_from_player(Board.new)).to be 3
+
+    expect(writer_spy).to have_received(:ask_for_next_move).twice
   end
 
   it "reprompts move from user when number is outside grid" do
     allow(reader_spy).to receive(:get_input).and_return("32", "7")
-    expect(writer_spy).to receive(:ask_for_next_move).twice
 
     expect(command_line_ui.get_move_from_player(Board.new)).to be 7
+
+    expect(writer_spy).to have_received(:ask_for_next_move).twice
   end
 
   it "reprompts move from user when an occupied position is entered" do
     allow(reader_spy).to receive(:get_input).and_return("1", "2")
     board = Board.new([PlayerSymbols::X, nil, nil, nil, nil, nil, nil, nil, nil])
-    
+
     expect(command_line_ui.get_move_from_player(board)).to be 2
   end
 
   it "reads replay option Y" do
     allow(reader_spy).to receive(:get_input).and_return("Y")
-    expect(writer_spy).to receive(:replay).once
-    
+
     expect(command_line_ui.replay?).to be true 
+
+    expect(writer_spy).to have_received(:replay)
   end
 
   it "reads replay option y" do
     allow(reader_spy).to receive(:get_input).and_return("y")
-    
+
     expect(command_line_ui.replay?).to be true 
   end
 
   it "replay is false when non valid option entered" do
     allow(reader_spy).to receive(:get_input).and_return("Z")
-    expect(writer_spy).to receive(:replay)
-  
+
     expect(command_line_ui.replay?).to be false 
+
+    expect(writer_spy).to have_received(:replay)
   end
 
   it "prints winning game status" do
     winning_board = Board.new([PlayerSymbols::X, PlayerSymbols::X, PlayerSymbols::X, nil, nil, nil, nil, PlayerSymbols::O, PlayerSymbols::O])
-    expect(writer_spy).to receive(:show_winning_message)
-    
+
     command_line_ui.print_game_status(winning_board)  
+
+    expect(writer_spy).to have_received(:show_winning_message)
   end
 
   it "prints winning board" do
     winning_board = Board.new([PlayerSymbols::X, PlayerSymbols::X, PlayerSymbols::X, nil, nil, nil, nil, PlayerSymbols::O, PlayerSymbols::O])
-    expect(writer_spy).to receive(:show_board)
-    
+
     command_line_ui.print_game_status(winning_board)  
+
+    expect(writer_spy).to have_received(:show_board)
   end
 
   it "prints draw game status" do
     drawn_board = Board.new([PlayerSymbols::X, PlayerSymbols::X, PlayerSymbols::O, PlayerSymbols::O, PlayerSymbols::X, PlayerSymbols::X, PlayerSymbols::X, PlayerSymbols::O, PlayerSymbols::O])
-    expect(writer_spy).to receive(:show_draw_message)
-    
+
     command_line_ui.print_game_status(drawn_board)  
+
+    expect(writer_spy).to have_received(:show_draw_message)
   end
 end
